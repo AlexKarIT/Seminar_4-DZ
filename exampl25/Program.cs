c5b81de3ead6fecf3a62d4dc9c9fee089db4ace6
Alex <akaravashkin5@gmail.com>
2022-12-10 16:17:08 +0300

    DZ

---
+++ exampl25/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+int degree(int X, int Y)
+{
+int rezult = 1;
+for (int i = 0; i < Y; i++)
+{
+    rezult = rezult * X;
+}
+return rezult;
+}
+
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень B , в которую возвести А: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Результат возведения в степень числа А в степень В: ");
+Console.WriteLine( degree(A, B));
---
+++ exampl29/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] GetArray(int N, int M)
+{
+    int[] array = new int[N];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, M+1);
+    }
+
+    return array;
+}
+
+
+int L = 8;
+
+Console.WriteLine("Введите максимальное значение, которое может принять каждый элемент:");
+int max = Convert.ToInt32(Console.ReadLine());
+
+var str = string.Join(", ", GetArray(L, max));
+Console.WriteLine("Полученный массив из " + L + " случайных чисел от 1 до " + max + " (включительно): ");
+Console.WriteLine("[" + str + "]");
